749b85eb613866a2cffd6deea41c6a4431c529cd
Antoine Le Blevec <leblevecantoine@gmail.com>
2019-04-09 10:22:58 +0200

    correction des erreurs dans le calcul des données et dans le vmin et vmax de la colorbar

--- snapshot.py
+++ snapshot.py
@@ -4,22 +4,17 @@
 Created on Wed Apr  3 11:08:51 2019
 
 @author: antoineleblevec
-
-amélioration du read_file pour rajouter la carte sur le hexbin.plot 
+ 
 """
-
 import os
 import pandas as pd
 from functools import reduce 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from scipy.signal import savgol_filter
-from matplotlib.font_manager import FontProperties
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
 
-import fonction_1 as f1  
+import fonctions as f1  
 
 # =============================================================================
 # Constantes 
@@ -29,52 +24,34 @@ H = 350e3
 lllat = 31.7; urlat = 43.7; lllon = 130.6; urlon = 145.6
 window = 27
 order = 4
-stations = ('auck','bluf','chti','dnvk','dund','gisb','gldb','haas','hamt','hast','hikb',
-            'hoki','kaik','ktia','lexa','lkta','maho','mavl','meth','mqzg','mtjo','nlsn',
-            'nply','pygr','taup','trng','vgmt','waim','wang','wark','west','wgtn','whkt',
-            'whng','wrpa')
+epoque1 = 1325
+epoque2 = 1367
 
 # =============================================================================
-# Lecture du dossier comprenant les différents fichiers 
+# Lecture du répertoire ; construction d'une liste avec nom des stations
 # =============================================================================
 directory = os.path.join('/Users/antoineleblevec/Desktop/G20')
 rep = os.path.abspath(os.path.expanduser(directory))
 files = os.listdir(rep) 
 files.sort()
 os.chdir(directory)
-
+stations = []
+for a in range (len(files)): 
+    name = files[a].split('_')
+    stations.append(name[0])
 # =============================================================================
-# Fonction de lecture de mon dataframe 
-# =============================================================================
-def read(files):
-    df = pd.read_csv(
-            files,
-            skiprows=11,
-            delim_whitespace=True,
-            header=None,
-            names=["tsn", "hour", "el", "az", "tec", "tec.p1p2", "validity"]
-            )
-    
-    df = df[["tsn", "el", "az", "tec"]]
-    
-    df = df[2320 < df["tsn"]]
-    return df[df["tsn"] < 2593]
-
-# =============================================================================
-# Concaténation des fichiers de chaque station dans un dataframe en fonction de 
-# l'index correspondant aux époques 
+# Liste de DataFrame où chaque élément correspond aux données d'une station
 # =============================================================================
 df = []
-
 for i in range(len(files)) : 
-    df.append(read(files[i]))
-for j in range (len(df)) : 
-    df[j] = df[j].set_index("tsn")
+    df.append(f1.read(files[i],epoque1,epoque2))
+    df[i] = df[i].set_index("tsn")
+    df[i].columns = ['el_{0}'.format(stations[i]), 'az_{0}'.format(stations[i]), 'tec_{0}'.format(stations[i])]
 dftot = reduce(lambda x, y: pd.merge(x, y, on = "tsn"), df)
-
-# =============================================================================
-# Lecture de la lon et lat de chaque stations
-# =============================================================================
+#
+##### =============================================================================
+##### Lon et Lat des Stations 
+##### =============================================================================
 rlon = []
 rlat = []
 for k in range(len(files)) :
@@ -82,86 +59,59 @@ for k in range(len(files)) :
     rlat.append(f1.lecture_lat(files[k]))
 df_rlat = pd.DataFrame([rlat])
 df_rlon = pd.DataFrame([rlon])
+#
+##### =============================================================================
+##### Calcul du tec réduit, de la latitude et de la longitude 
+##### =============================================================================
+df_el = pd.DataFrame()
+df_az = pd.DataFrame()
+df_tec = pd.DataFrame()
+for j in range(len(files)):
+    df_el = df_el.append(dftot['el_{0}'.format(stations[j])])
+    df_az = df_az.append(dftot['az_{0}'.format(stations[j])])
+    df_tec = df_tec.append(dftot['tec_{0}'.format(stations[j])])
 
-# =============================================================================
-# Calcul du tec réduit, de la latitude et de la longitude 
-# =============================================================================
-df_el = dftot.el
-df_elx = dftot.el_x
-df_ely = dftot.el_y
-df_ele = df_elx.add(df_ely, fill_value=0)
-df_ele['el'] = pd.Series(df_el)
-df_ele = df_ele.rename(index=str, columns={"el_x":"ele", "el_y":"ele", "el":"ele"})
-df_ele = np.radians(df_ele)
+df_el = np.radians(df_el)
+df_az = np.radians(df_az)
 
-df_az = dftot.az
-df_azx = dftot.az_x
-df_azy = dftot.az_y
-df_azi = df_azx.add(df_azy, fill_value=0)
-df_azi['az'] = pd.Series(df_az)
-df_azi = df_azi.rename(index=str, columns={"az_x":"azi", "az_y":"azi", "az":"azi"})
-df_azi = np.radians(df_azi)
+df_ele = df_el.T
+df_azi = df_az.T
+df_tecr = df_tec.T
 
 df_x = np.arcsin((Re * np.cos(df_ele)) / (Re + H))
-df_x = df_x.rename(index=str, columns={"x" : "ele"})
-
 df_ksi = (np.pi / 2) - (df_ele.add((df_x), fill_value=0))
-df_ksi = df_ksi.rename(index=str, columns={'ele':'ksi'})
 
-# étapes de calcul nécessaire à cause des opérations entre dataframe
+#### étapes intermédiaire de calcul
 tplat = pd.np.multiply(np.sin(df_ksi),np.cos(df_azi))
 tplon = pd.np.multiply(np.sin(df_ksi),np.sin(df_azi))
-
+####
 df_lat = np.arcsin(pd.np.multiply(np.cos(df_ksi),np.sin(df_rlat)) + 
                    pd.np.multiply(tplat,np.cos(df_rlat)))
-df_lat = df_lat.rename(index=str, columns={'ksi':'lat'})
-
+#### étape intermédiaire de calcul
 tpplon = np.arcsin(pd.np.divide(tplon,np.cos(df_lat)))
+####
 df_lon = pd.np.add(tpplon,df_rlon)
-df_lon = df_lon.rename(index=str, columns={'ksi':'lon'})
 
-df_tec = dftot.tec
-df_tecx = dftot.tec_x
-df_tecy = dftot.tec_y
-df_tecr = df_tecx.add(df_tecy, fill_value = 0)
-df_tecr['tec'] = pd.Series(df_tec)
-df_tecr = df_tecr.rename(index=str, columns={"tec_x" : "tecr", "tec_y" : "tecr", "tec" : "tecr"})
-
-df_tecr = df_tecr - df_tecr.apply(f1.minimum,axis=0)
-
-df_vtec = pd.np.multiply(df_tecr,np.cos(df_x))
-df_vtecf = savgol_filter(df_vtec,window,order)
+df_tecr_reduit = df_tecr - df_tecr.apply(f1.minimum,axis=0)
+df_vtec = pd.np.multiply(df_tecr_reduit,np.cos(df_x))
+df_vtecf = savgol_filter(df_vtec,window,order,axis=0)
 df_vtecr = df_vtec - df_vtecf
-
-# =============================================================================
-# création de tableau par époques 
-# =============================================================================
-# index commence à 0 
-df_lon.reset_index(drop = True, inplace = True)
-df_lat.reset_index(drop = True, inplace = True)
-df_vtecr.reset_index(drop = True, inplace = True)
-
-# rename columns en fonction des stations
-df_lon.columns = stations
-df_lat.columns = stations
-df_vtecr.columns = stations
-#print (df_vtecr)
-
-# valeurs tec : -1.5 -> 0.8
+##
+###### =============================================================================
+###### plot valeurs tec : -.5 -> 0.6
+###### =============================================================================
 fig = plt.figure()
-
-for i in range (1, 8):
-    a = i * 10
-    lon = np.degrees(df_lon.iloc[a:a+1].values.flatten())
-    lat = np.degrees(df_lat.iloc[a:a+1].values.flatten())
-    tec = df_vtecr.iloc[a:a+1].values.flatten()
+for i in range (1,23):
+    lon = np.degrees(df_lon.iloc[i:i+1].values.flatten())
+    lat = np.degrees(df_lat.iloc[i:i+1].values.flatten())
+    tec = df_vtecr.iloc[i:i+1].values.flatten()
     
     data = {'lon' : lon,
             'lat' : lat,
             'tec': tec}
     frame = pd.DataFrame(data)
     
-    fig.add_subplot(2,4,i)
+    fig.add_subplot(4,6,i)
     m = f1.basic_nz_map()
     x, y = m(lon, lat)
     m.hexbin(x,
@@ -170,19 +120,11 @@ for i in range (1, 8):
              reduce_C_function=np.mean,
              gridsize=20, 
              cmap="viridis", 
-             vmin=-1.5, vmax=0.8)
-    plt.title('tec at epoque {0}'.format(a))
+             vmin=-0.26, vmax=0.25)
+    plt.title('tec at epoque {0}'.format(epoque1+i))
 
+#positionnement de la colorbar
 cbaxes = fig.add_axes([0.94, 0.1, 0.01, 0.8]) 
 cb = plt.colorbar(cax = cbaxes)
 m.colorbar()
 plt.show()
-
-
-
-
-
-
-
-
-
